6bef8cea8056ad8fefc942c3d3bf033d119ac52e
Jan Zabloudil <zabloudil@softmakers.cz>
2021-02-16 09:36:07 +0100

    unique slug

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -47,7 +47,7 @@ class Company implements UserInterface
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $slug;
 
