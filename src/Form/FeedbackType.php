de485b39269cc8f5c3ddc1841d580b11db9a4b06
Jan Zabloudil <zabloudil@softmakers.cz>
2021-02-16 20:49:19 +0100

    Edit feedback form added

---
+++ src/Form/FeedbackType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Feedback;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FeedbackType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description', TextareaType::class)
+            ->add('source', TextareaType::class, array('required' => false))
+            ->add('save', SubmitType::class, ['label' => 'Create'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Feedback::class,
+        ]);
+    }
+}
