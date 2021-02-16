5ec6e39a85481f85f2a0089b3a4e6fa3d0d38da9
Jan Zabloudil <zabloudil@softmakers.cz>
2021-02-16 21:54:15 +0100

    Feedback list added, feedback voter added

--- src/Security/Voter/BackOfficeFeedbackVoter.php
+++ src/Security/Voter/BackOfficeFeedbackVoter.php
@@ -6,20 +6,21 @@ use App\Entity\Company;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\Feedback;
 
-class BackOfficeVoter extends Voter
+class BackOfficeFeedbackVoter extends Voter
 {
     protected function supports($attribute, $subject)
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
         return in_array($attribute, ['edit'])
-            && $subject instanceof Company;
+            && $subject instanceof Feedback;
     }
 
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
-        /** @var Company $subject */
+        /** @var Feedback $subject */
 
         $company = $token->getUser();
         // if the user is anonymous, do not grant access
@@ -28,9 +29,10 @@ class BackOfficeVoter extends Voter
         }
 
         // ... (check conditions and return true to grant permission) ...
+        $feedback = $subject;
         switch ($attribute) {
             case 'edit':
-                if($subject === $company){
+                if($feedback->getCompany() === $company){
                     return true;
                 }
                 break;
