2b8765774c4b557ecf229f561310736634a8c341
Jan Zabloudil <zabloudil@softmakers.cz>
2021-02-17 16:44:18 +0100

    Fix use

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -11,6 +11,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class BackOfficeController extends AbstractController
 {
