cd0c689c9c7f9d5b7a5ae166c866663b9cb15e3a
Jan Zabloudil <zabloudil@softmakers.cz>
2021-02-17 21:34:44 +0100

    Features fixtures added

--- migrations/Version20210217203135.php
+++ migrations/Version20210217203135.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210217182502 extends AbstractMigration
+final class Version20210217203135 extends AbstractMigration
 {
     public function getDescription() : string
     {
