e5e11ce83f9f0a9049e79262c869e96a563b8c62
Jan Zabloudil <zabloudil@softmakers.cz>
2021-02-19 12:00:40 +0100

    Small refactoring

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -106,8 +106,7 @@ class BackOfficeController extends AbstractController
             $entityManager->persist($feedback);
             $entityManager->flush();
 
-            $event = new FeedbackUpdatedEvent();
-            $this->dispatcher->dispatch($event, 'feedback.updated.event');
+            $this->dispatchFeedbackUpdatedEvent();
 
             return $this->redirectToRoute('feedback-list',[
                 'slug' => $company->getSlug()
@@ -154,8 +153,7 @@ class BackOfficeController extends AbstractController
 
             $entityManager->flush();
 
-            $event = new FeedbackUpdatedEvent();
-            $this->dispatcher->dispatch($event, 'feedback.updated.event');
+            $this->dispatchFeedbackUpdatedEvent();
 
             $this->addFlash('success', 'Feedback updated');
         }
@@ -198,8 +196,7 @@ class BackOfficeController extends AbstractController
         $entityManager->remove($feedback);
         $entityManager->flush();
 
-        $event = new FeedbackUpdatedEvent();
-        $this->dispatcher->dispatch($event, 'feedback.updated.event');
+        $this->dispatchFeedbackUpdatedEvent();
 
         return $this->redirectToRoute('feedback-list',[
             'slug' => $company->getSlug()
@@ -312,7 +309,6 @@ class BackOfficeController extends AbstractController
     {
 
         $this->denyAccessUnlessGranted('edit', $feature);
-        dump( $this->getDoctrine()->getRepository(Feedback::class)->getFeedbackCountForFeature($company, $feature));
 
         $entityManager = $this->getDoctrine()->getManager();
 
@@ -396,6 +392,16 @@ class BackOfficeController extends AbstractController
             'feature' => $feature,
             'companySlug' => $company->getSlug()
         ]);
+    }
+
+    private function dispatchFeedbackUpdatedEvent()
+    {
+
+       return $this->dispatcher->dispatch(
+           new FeedbackUpdatedEvent(),
+           'feedback.updated.event'
+       );
+
     }
 
 
