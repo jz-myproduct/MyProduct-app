33df2f4a0cb555f038f2090144e92387ffb7c3e2
Jan Zabloudil <zabloudil@softmakers.cz>
2021-02-19 15:17:46 +0100

    Adding feedback on feature detail

---
+++ src/Form/FeedbackFeatureDetailFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Feedback;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FeedbackFeatureDetailFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description', TextareaType::class)
+            ->add('source', TextareaType::class, array('required' => false))
+            ->add('save', SubmitType::class, ['label' => 'Přidat']);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Feedback::class,
+        ]);
+    }
+}
