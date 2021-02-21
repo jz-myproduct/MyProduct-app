7d4ade54bdf297fb545aad1d6210b11e9617fba4
Jan Zabloudil <zabloudil@softmakers.cz>
2021-02-21 20:18:16 +0100

    Controllers' namespaces updated

--- src/Controller/BackOffice/BackOfficeController.php
+++ src/Controller/BackOffice/BackOfficeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\BackOffice;
 
 use App\Entity\Company;
 
--- src/Controller/FrontOffice/FrontOfficeController.php
+++ src/Controller/FrontOffice/FrontOfficeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\FrontOffice;
 
 use App\Entity\FeatureState;
 use App\Services\SlugService;
