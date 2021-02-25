d0acf5dca2dd7745e4536308ad56fafdb95527ce
Jan Zabloudil <zabloudil@softmakers.cz>
2021-02-25 19:51:12 +0100

    Roles update

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -126,8 +126,6 @@ class Company implements UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
