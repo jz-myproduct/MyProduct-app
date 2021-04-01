8600bc3d40ab37ea38b30f2bad29c5525619f05a
Jan Zabloudil <zabloudil@softmakers.cz>
2021-04-01 10:34:09 +0200

    Typo fix

--- migrations/Version20210331080930.php
+++ migrations/Version20210331080930.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210327101844 extends AbstractMigration
+final class Version20210331080930 extends AbstractMigration
 {
     public function getDescription() : string
     {
